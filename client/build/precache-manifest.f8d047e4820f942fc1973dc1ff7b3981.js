39e1c957b8b4eaf244c984b0aceff0567b72c179
GITWizard3000 <72543669+GITWizard3000@users.noreply.github.com>
2020-11-02 11:57:42 +0200

    Add files via upload

---
+++ client/build/precache-manifest.f8d047e4820f942fc1973dc1ff7b3981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a522525c0c8d614591ad1fd904193853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ea58eeedaaeb6e0d6f",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "d6bb1920cf57628eafb0",
+    "url": "/static/js/2.6dac5f75.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.6dac5f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ea58eeedaaeb6e0d6f",
+    "url": "/static/js/main.b74d3f68.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
